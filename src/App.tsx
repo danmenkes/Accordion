6fe04490d80eb722646393d8eb5d497d8db3768a
dan1-menkes1 <dan.menkes@guidde.co>
2024-02-18 17:03:45 +0200

    image slider

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Accordion from "./components/accordion/Accordion";
 import RandomColor from "./components/random-color/RandomColor";
 import StarRating from "./components/star-rating/StarRating";
+import { ImageSlider } from "./components/imgae-slider/ImageSlider";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <RandomColor />
       <br />
       <StarRating />
+      <br />
+      <ImageSlider limit={10} url="https://picsum.photos/v2/list" />
     </div>
   );
 }
